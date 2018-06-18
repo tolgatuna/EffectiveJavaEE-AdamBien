0d8526c96fc35fd34b9a1e1cc41e17e0c59a1daa
tolga.tuna <tolgatuna26@gmail.com>
2018-06-19 00:45:10 +0300

    05-JAX-RS Rules For JUnit-HD is completed.

--- doitrs/src/main/java/com/tt/doit/business/reminder/TodoResourceIT.java
+++ doitrs/src/main/java/com/tt/doit/business/reminder/TodoResourceIT.java
@@ -1,11 +1,9 @@
 package com.tt.doit.business.reminder;
 
-import org.junit.Before;
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
+import org.junit.Rule;
 import org.junit.Test;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -14,18 +12,27 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class TodoResourceIT {
+
+/*
     private Client client;
     private WebTarget webTarget;
+*/
 
+    // It just an alternative way to initialize JAX-RS Client
+    @Rule
+    public JAXRSClientProvider jaxrsClientProvider = JAXRSClientProvider.buildWithURI("http://localhost:8083/doit/api/todo");
+
+/*
     @Before
     public void initClient() {
         this.client = ClientBuilder.newClient();
         this.webTarget = this.client.target("http://localhost:8083/doit/api/todo");
     }
+*/
 
     @Test
     public void fetchTodo() {
-        Response response = this.webTarget.request(MediaType.TEXT_PLAIN).get();
+        Response response = this.jaxrsClientProvider.target().request(MediaType.TEXT_PLAIN).get();
         assertThat(response.getStatus(), is(200));
         String readEntity = response.readEntity(String.class);
         assertTrue(readEntity.startsWith("Hello"));
